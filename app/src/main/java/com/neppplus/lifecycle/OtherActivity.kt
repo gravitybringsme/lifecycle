2f31f7e08bfe86d420c0fc4fbf63aae7db2e7ec5
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-23 18:41:05 +0900

    life cycle onCreate/onResume

--- app/src/main/java/com/neppplus/lifecycle/OtherActivity.kt
+++ app/src/main/java/com/neppplus/lifecycle/OtherActivity.kt
@@ -1,35 +1,23 @@
 package com.neppplus.lifecycle
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_other)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        Log.d("메인화면", "onCreate실행됨")
-
-        findViewById<Button>(R.id.moveToOtherBtn).setOnClickListener{
-            val myIntent = Intent(this, OtherActivity::class.java)
-            startActivity(myIntent)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("메인화면", "onReume 실행됨")
+        Log.d("다른화면", "onCreate실행됨")
     }
 }
